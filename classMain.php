049a8cdc32cc423fb36a61950af89b423fca89d9
Asher Fleisher <asherfleisherr@gmail.com>
2017-11-09 15:08:25 +0200

    Update classMain.php

--- classMain.php
+++ classMain.php
@@ -4,16 +4,15 @@
         function go() 
         {
             require_once "DAL.php";
-            $stmt = $pdo->query("SELECT * FROM lumbergo.model_steps;");
+            $stmt = $pdo->query("SELECT * FROM model_steps;");
             foreach ($stmt as $row)
             {     
                 if($_COOKIE["pagecookie"] == $row["step_id"] )
                 {  
                     echo "<span id='info'><img src='images/info.png' height='42px' width='42px'><h5>" . $row['texti'] . $row['filename'] . "</h5></span>" ."<span id='stepNumberSpan'>" . $row['step_id'] . "</span><br/>";  
-                    echo "<script >var image=('./images/' . {$row['filename']}); var is3d={$row['is3D']}; varviewAngle={$row['viewAngle']};</script>";
+                    echo "<script >var image=('./images/' . {$row['filename']}); var is3d={$row['is3D']}; var viewAngle={$row['viewAngle']};</script>";
                 }
-            };
+            }
         }        
     }
-
 ?>
